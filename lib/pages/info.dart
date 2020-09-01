b1bdb93ac3a54af8f13fc66cccc9aac92a3ed763
Rafael <rfael403@gmail.com>
2020-09-01 00:03:35 -0300

    created info page

---
+++ lib/pages/info.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class info extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sobre"),
+      ),
+      body: Center(
+        child: Column(
+            children: [
+              Text("Desenvolvido por:", style: TextStyle(fontSize: 20),),
+              Image.asset('assets/images/ore.png', height: 80, width: 80,),
+              Text("O.R.E", style: TextStyle(fontSize: 30),),
+              Text("Versão 1.0.0"),
+            ]
+        ),
+      ),
+    );
+  }
+}
