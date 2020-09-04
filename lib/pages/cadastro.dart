1c60d2261b098bb78a5a36340e4a5caa86a49e2b
Rafael <rfael403@gmail.com>
2020-09-04 19:56:00 -0300

    update cadastro

--- lib/pages/cadastro.dart
+++ lib/pages/cadastro.dart
@@ -8,16 +8,63 @@ class cadastro extends StatelessWidget {
         title: Text("Cadastrar"),
       ),
       body: Center(
-        child: Column(
+        child: ListView(
             children: [
               TextFormField(
+                autofocus: true,
                 decoration: InputDecoration(
+                  icon: Icon(Icons.cake),
+                  border: InputBorder.none,
                   labelText: "Nome do Produto",
                 ),
               ),
               TextFormField(
                 decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  border: InputBorder.none,
                   labelText: "Nome do Cliente",
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.attach_money),
+                  border: InputBorder.none,
+                  labelText: "Preço do produto",
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.call_to_action),
+                  border: InputBorder.none,
+                  labelText: "Forma de pagamento",
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.place),
+                  border: InputBorder.none,
+                  labelText: "Endereço do cliente",
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.calendar_today),
+                  border: InputBorder.none,
+                  labelText: "Data da entrega",
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.call),
+                  border: InputBorder.none,
+                  labelText: "Telefone",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.content_paste),
+                  border: InputBorder.none,
+                  labelText: "Obeservações",
                 ),
               ),
             ]
